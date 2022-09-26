a48303df71c68319ba6d3b93af83ed2ad6495530
Arvid Borensved <arvid.borensved@utb.ecutbildning.se>
2022-09-26 11:35:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Home from './views/home/home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HEJ ARVID</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/views/home/home.js
@@ -0,0 +1,33 @@
+import Select from "react-select";
+
+const Home = () => {
+
+  const options = [
+    {
+      value: 'Default',
+      label: 'Please choose an option'
+    },
+    {
+      value: 'room1',
+      label: 'Room 1'
+    },
+    {
+      value: 'room2',
+      label: 'Room 2'
+    },
+    {
+      value: 'room3',
+      label: 'Room 3'
+    },
+  ]
+
+  return <div>
+    <div>
+      <input/>
+      <Select options={options}/>
+      <button>hejsan222</button>
+    </div> 
+  </div>
+}
+
+export default Home;
